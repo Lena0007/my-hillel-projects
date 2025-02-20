68de5bf0c3e3065fb309f0d2797a36730fed5645
Lena0007 <122921197+Lena0007@users.noreply.github.com>
2025-02-20 18:56:40 +0100

    Update 4_1.py

--- 4_1.py
+++ 4_1.py
@@ -1,3 +1,12 @@
-print ("Hello World")
-m = 2 + 1 + 1
-print (m)
+def move_zeros(list):
+    non_zeros = []
+    zeros = []
+
+    for num in  list:
+        if num == 0:
+            zeros.append(num)
+        else:
+            non_zeros.append(num)
+    return non_zeros + zeros
+
+print(move_zeros([9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0]))
